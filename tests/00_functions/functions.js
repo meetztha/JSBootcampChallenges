875150209947b80750b00cb57d563c49de7b21c8
Meetztha <meethath@gmail.com>
2023-03-18 23:10:31 -0500

    Completed Max function

--- tests/00_functions/functions.js
+++ tests/00_functions/functions.js
@@ -1,8 +1,26 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable consistent-return */
-function max() {
-return 10
+function max(a,b,c) {
+  
+    max_val = 0;
+    if (a>b) 
+    {
+        max_val = a;
+    } else
+     {
+        max_val = b;
+    }
+    if (c>max_val)
+    {
+        max_val = c;
+    }
+    return max_val;
 }
+console.log(max(4,5,6))
+console.log(max(10,8,6))
+console.log(max(20,40,10))
+
+
 
 function addUnknown() {
 
